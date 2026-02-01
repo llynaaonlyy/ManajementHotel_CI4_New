fa14751df5f3abcae4de195bea152871968f2b54
ana? <anajariyatunkhasanah1@gmail.com>
2026-02-01 09:52:46 +0700

    update file UserModel.php

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -13,28 +13,28 @@ class UserModel extends Model
     protected $createdField = 'created_at';
     protected $updatedField = 'updated_at';
     
-    protected $validationRules = [
-        'nama' => 'required|min_length[3]',
-        'email' => 'required|valid_email|is_unique[users.email,id,{id}]',
-        'no_telp' => 'permit_empty|numeric|min_length[10]|max_length[15]',
-        'role' => 'permit_empty|in_list[pelanggan,pegawai,admin]'
-    ];
+    // protected $validationRules = [
+    //     'nama' => 'required|min_length[3]',
+    //     'email' => 'required|valid_email|is_unique[users.email,id,{id}]',
+    //     'no_telp' => 'permit_empty|numeric|min_length[10]|max_length[15]',
+    //     'role' => 'permit_empty|in_list[pelanggan,pegawai,admin]'
+    // ];
     
-    protected $validationMessages = [
-        'nama' => [
-            'required' => 'Nama harus diisi',
-            'min_length' => 'Nama minimal 3 karakter'
-        ],
-        'email' => [
-            'required' => 'Email harus diisi',
-            'valid_email' => 'Email tidak valid',
-            'is_unique' => 'Email sudah terdaftar'
-        ],
-        'no_telp' => [
-            'numeric' => 'No. telepon harus berupa angka',
-            'min_length' => 'No. telepon minimal 10 digit'
-        ]
-    ];
+    // protected $validationMessages = [
+    //     'nama' => [
+    //         'required' => 'Nama harus diisi',
+    //         'min_length' => 'Nama minimal 3 karakter'
+    //     ],
+    //     'email' => [
+    //         'required' => 'Email harus diisi',
+    //         'valid_email' => 'Email tidak valid',
+    //         'is_unique' => 'Email sudah terdaftar'
+    //     ],
+    //     'no_telp' => [
+    //         'numeric' => 'No. telepon harus berupa angka',
+    //         'min_length' => 'No. telepon minimal 10 digit'
+    //     ]
+    // ];
     
     // Get user by email
     public function getUserByEmail($email)
