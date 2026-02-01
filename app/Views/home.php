51845df0c3efd9dda3fcb9fd2a473dcd0d595b27
ana? <anajariyatunkhasanah1@gmail.com>
2026-02-01 10:39:38 +0700

    update file home.php

--- app/Views/home.php
+++ app/Views/home.php
@@ -85,7 +85,7 @@
         }
         
         .profil-icon {
-            margin-left: 15px;
+            padding-left: 15px;
             width: 45px;
             height: 45px;
             border-radius: 50%;
